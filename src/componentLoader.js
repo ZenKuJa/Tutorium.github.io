5c9abe456b236c4f81c89702511f1e89cfdd2a52
ZenKuJa <janneskurzke@gmail.com>
2025-05-02 14:23:39 +0200

    trying to fix paths 9

--- src/componentLoader.js
+++ src/componentLoader.js
@@ -18,7 +18,7 @@ async function loadComponent(url, targetId) {
   }
   
   document.addEventListener('DOMContentLoaded', () => {
-    loadComponent('components/footer.html', 'footer-container');
-    loadComponent('components/navbar.html' , 'navbar-container');
-    loadComponent('components/header.html', 'header-container');
+    loadComponent('/components/footer.html', 'footer-container');
+    loadComponent('/components/navbar.html' , 'navbar-container');
+    loadComponent('/components/header.html', 'header-container');
   });
